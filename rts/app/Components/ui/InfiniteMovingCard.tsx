bb7b87fc0ee109a578a9b3fcfeccaec495e0121f
Rudraa Tech Solution <rudraatechsolution@gmail.com>
2024-06-22 21:25:21 +0530

    Update InfiniteMovingCard.tsx

--- rts/app/Components/ui/InfiniteMovingCard.tsx
+++ rts/app/Components/ui/InfiniteMovingCard.tsx
@@ -27,7 +27,7 @@ export const InfiniteMovingCard = ({
 
   useEffect(() => {
     addAnimation();
-  }, []);
+  }, [addAnimation]);
   const [start, setStart] = useState(false);
   function addAnimation() {
     if (containerRef.current && scrollerRef.current) {
