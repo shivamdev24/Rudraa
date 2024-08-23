2e54f3742bb7217178e1982b1b9431604669114c
shivamdev24 <shivam.dev2402@gmail.com>
2024-08-24 00:18:13 +0530

    reset Project

--- rts/next.config.mjs
+++ rts/next.config.mjs
@@ -1,12 +1,4 @@
-// @ts-check
- 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    images: {
-        domains: ['cdn-icons-png.freepik.com'],
-      },
-  }
-   
-  export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
